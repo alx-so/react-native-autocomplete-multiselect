d90f5f2ff3db024e7e845b668a1584072d830f5b
alx-so <sokol.alxs@gmail.com>
2024-11-28 20:26:02 +0300

    feat(settings): confirmTagDelete, tagBackspaceDeleteBehavior

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -6,8 +6,17 @@ export interface ISettings {
    *
    * - `delete`: Remove the last tag.
    * - `delete-modify`: Remove the last tag and set the input value to the removed tag.
+   * - `delete-confirm`: Show a confirmation alert before deleting the last tag.
    *
    * `default`: 'delete'
    */
-  tagBackspaceDeleteBehavior?: 'delete' | 'delete-modify' | 'delete-con';
+  tagBackspaceDeleteBehavior?: 'delete' | 'delete-modify' | 'delete-confirm';
+
+  /**
+   * TODO: provide a way to show a confirmation alert before deleting the last tag.
+   * TODO: provide a way to customize the alert message and buttons.
+   *
+   * Use system alert to confirm tag deletion.
+   */
+  confirmTagDelete?: boolean;
 }
