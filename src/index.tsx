cc6e61df588080e5a47904765ad8cf322f149ff8
alx-so <sokol.alxs@gmail.com>
2024-12-02 19:42:04 +0300

    feat(autocomplete): implement layout change listener to pass for adjusting dropdown position

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,27 @@
+import { type LayoutRectangle, StyleSheet, View, type LayoutChangeEvent } from 'react-native';
 import { Input } from './components/Input';
 import { defaultSettings } from './defaultSettings';
 import type { ISettings } from './types/settings';
+import { Dropdown } from './components/Dropdown';
+import React from 'react';
+import { defaultLayloutRect, getTestSearchItems, mergeSettings } from './utils';
 
 export const AutoComplete = (settings: ISettings = defaultSettings) => {
-  settings = { ...defaultSettings, ...settings };
+  const containerRef = React.useRef<View>(null);
+  const [containerRect, setContainerRect] = React.useState<LayoutRectangle>(defaultLayloutRect);
 
-  return <Input {...settings} />;
+  const handleContainerLayoutChange = (e: LayoutChangeEvent) => {
+    setContainerRect(e.nativeEvent.layout);
+  };
+
+  return (
+    <View style={styles.container} ref={containerRef} onLayout={handleContainerLayoutChange}>
+      <Input {...mergeSettings(defaultSettings, settings)} />
+      <Dropdown items={getTestSearchItems()} containerRect={containerRect} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {},
+});
