faeacdb37a4da24302c59b0cfb09cb1be9c97c5a
alx-so <sokol.alxs@gmail.com>
2024-12-02 21:18:38 +0300

    feat(autocomplete): lift input value change to parent, cache settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { type LayoutRectangle, StyleSheet, View, type LayoutChangeEvent } from 'react-native';
-import { Input } from './components/Input';
+import { InputMemoized } from './components/Input';
 import { defaultSettings } from './defaultSettings';
 import type { ISettings } from './types/settings';
 import { Dropdown } from './components/Dropdown';
@@ -9,14 +9,24 @@ import { defaultLayloutRect, getTestSearchItems, mergeSettings } from './utils';
 export const AutoComplete = (settings: ISettings = defaultSettings) => {
   const containerRef = React.useRef<View>(null);
   const [containerRect, setContainerRect] = React.useState<LayoutRectangle>(defaultLayloutRect);
+  // const [inputValue, setInputValue] = React.useState<string>('');
+  // const [isPedingInputValue, startInputValueTransition] = useTransition();
+  const inputSettingsProps = React.useMemo(
+    () => mergeSettings(defaultSettings, settings),
+    [settings]
+  );
 
   const handleContainerLayoutChange = (e: LayoutChangeEvent) => {
     setContainerRect(e.nativeEvent.layout);
   };
 
+  // const handleInputChange = useCallback((text: string) => {
+  // startInputValueTransition(() => setInputValue(text));
+  // }, []);
+
   return (
     <View style={styles.container} ref={containerRef} onLayout={handleContainerLayoutChange}>
-      <Input {...mergeSettings(defaultSettings, settings)} />
+      <InputMemoized {...inputSettingsProps} />
       <Dropdown items={getTestSearchItems()} containerRect={containerRect} />
     </View>
   );
