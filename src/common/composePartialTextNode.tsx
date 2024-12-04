edd16e2457b85f04d604e5b816e7f872f9dfd469
alx-so <sokol.alxs@gmail.com>
2024-12-04 23:24:28 +0300

    feat(common): composePartialTextNode

---
+++ src/common/composePartialTextNode.tsx
@@ -0,0 +1,27 @@
+import { Text, type TextStyle } from 'react-native';
+
+export const composePartialTextNode = (str: string, opts: PartialTextNodeOpts) => {
+  const openTagStart = str.indexOf(opts.startStrPart);
+  const openTagEnd = str.indexOf(opts.endStrPart);
+  const startStrPart = str.slice(0, openTagStart);
+  const midStrPart = str.slice(openTagStart + opts.startStrPart.length, openTagEnd);
+  const endStrPart = str.slice(openTagEnd + 4);
+
+  if (openTagStart === -1 || openTagEnd === -1) {
+    return <Text>{str}</Text>;
+  }
+
+  return (
+    <Text>
+      {startStrPart}
+      <Text style={opts.matchedTextNodeStyle}>{midStrPart}</Text>
+      {endStrPart}
+    </Text>
+  );
+};
+
+export interface PartialTextNodeOpts {
+  startStrPart: string;
+  endStrPart: string;
+  matchedTextNodeStyle?: TextStyle;
+}
