91ea4826714f4c704bc9a2dd1ebcb183b134ecae
alx-so <sokol.alxs@gmail.com>
2024-12-07 21:10:28 +0300

    feat(settings); type

--- src/defaultSettings.ts
+++ src/defaultSettings.ts
@@ -1,6 +1,7 @@
 import type { Settings } from './types/settings';
 
 export const defaultSettings: Settings = {
+  type: 'input',
   blurOnSubmit: true,
   tagBackspaceDeleteBehavior: 'delete-modify',
   confirmTagDelete: false,
