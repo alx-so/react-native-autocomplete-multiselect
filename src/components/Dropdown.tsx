7a7a15476b929df4e22a390c797ac4c68c163691
alx-so <sokol.alxs@gmail.com>
2024-12-07 21:38:32 +0300

    refactor: add Dropdown search textInput, wrap items in Pressable to implement basic select item func

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -6,8 +6,10 @@ import {
   type ViewStyle,
   useWindowDimensions,
   type LayoutRectangle,
+  TextInput,
+  Pressable,
 } from 'react-native';
-import { Position } from '../types/common';
+import { Position, type Item } from '../types/common';
 import { composePartialTextNode } from '../common/composePartialTextNode';
 import { MATCH_TAG_END, MATCH_TAG_START } from '../constants';
 
@@ -19,40 +21,58 @@ export const Dropdown: DropdownComponent = (props) => {
   const position = calcDropdownPosition(props.containerRect, deviceDimensions.height);
   const containerStyle = getContainerStylePosition(position, props.containerRect.height);
 
+  const handleItemPress = (item: Item) => {
+    props.onItemPress?.(item);
+  };
+
   return (
     <View style={[styles.dropdown, containerStyle]}>
-      <ScrollView>
+      {props.isSearchVisible && (
+        <TextInput
+          style={styles.searchInput}
+          onChangeText={props.onSearchTextChange}
+          placeholder="Type to search.."
+        />
+      )}
+      <ScrollView style={styles.scrollView}>
         {props.items.map((item) => (
-          <View key={item.id}>
+          <Pressable key={item.id} onPress={() => handleItemPress(item)}>
             {composePartialTextNode(item.label, {
               matchedTextNodeStyle: { fontWeight: 'bold' },
               startStrPart: MATCH_TAG_START,
               endStrPart: MATCH_TAG_END,
             })}
-          </View>
+          </Pressable>
         ))}
       </ScrollView>
     </View>
   );
 };
 
-interface Item {
-  id: string | number;
-  label: string;
-}
-
 interface DropdownProps {
+  isSearchVisible?: boolean;
+  onSearchTextChange?: (text: string) => void;
   items: Item[];
   containerRect: LayoutRectangle;
+  onItemPress?: (item: Item) => void;
 }
 
 type DropdownComponent = React.FC<DropdownProps>;
 
 const styles = StyleSheet.create({
+  scrollView: {
+    maxHeight: requiredDropdownHeight / 2,
+  },
   dropdown: {
+    width: '100%',
     position: 'absolute',
     backgroundColor: 'rgba(0,0,0,0.1)',
-    width: '100%',
+  },
+  searchInput: {
+    height: 54,
+    backgroundColor: '#f9f9f9',
+    borderWidth: 1,
+    borderColor: 'green',
   },
 });
 
