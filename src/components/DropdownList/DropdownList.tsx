00d3660330e25de8986c4813f4b2cfed221f302e
alx-so <sokol.alxs@gmail.com>
2024-12-13 22:36:31 +0300

    refactor: DropdownList

--- src/components/DropdownList/DropdownList.tsx
+++ src/components/DropdownList/DropdownList.tsx
@@ -40,23 +40,11 @@ const styles = StyleSheet.create({
   scrollView: {
     maxHeight: maxDropdownHeight / 2,
   },
-  suggestionItem: {
-    paddingVertical: 5,
-    backgroundColor: 'rgba(0,0,0,0.1)',
-    borderBottomWidth: 0.5,
-    borderColor: 'grey',
-  },
   dropdown: {
     width: '100%',
     position: 'absolute',
     zIndex: 9999999999999,
     backgroundColor: 'white',
     borderWidth: 1,
-  },
-  searchInput: {
-    height: 54,
-    backgroundColor: '#f9f9f9',
-    borderWidth: 1,
-    borderColor: 'green',
   },
 });
