ca4187cf5d686554dd45b8c94d2f8959dd0377bb
alx-so <sokol.alxs@gmail.com>
2024-12-14 15:53:36 +0300

    feat: Select implement props onOpen, onClose, onChange

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Pressable, StyleSheet, View, type LayoutRectangle } from 'react-native';
 import ChevronIcon from './ChevronIcon';
 import { DropdownList } from './DropdownList';
@@ -6,25 +6,52 @@ import { defaultLayloutRect, getTestSearchItems } from '../utils';
 import { composePartialTextNode } from '../common/composePartialTextNode';
 import { MATCH_TAG_END, MATCH_TAG_START } from '../constants';
 
+interface SelectProps {
+  disabled?: boolean;
+  multiple?: boolean;
+  open?: boolean;
+  onOpen?: () => void;
+  onClose?: () => void;
+  onChange?: (value: string | string[]) => void;
+  placeholder?: string;
+  renderValue?: (value: string) => React.ReactNode;
+  testID?: string;
+  value?: string | string[];
+  icon?: React.ReactNode;
+}
+
 const iconSize = 12;
 
-export const Select: SelectComponent = (props) => {
+export const Select: React.FC<SelectProps> = (props) => {
+  const [isOpen, setIsOpen] = React.useState(props.open);
   const [containerRect, setContainerRect] = React.useState<LayoutRectangle>(defaultLayloutRect);
   const [value, setValue] = React.useState<string | string[]>(props.value || '');
+
+  useEffect(() => {
+    setIsOpen(props.open);
+  }, [props.open]);
+
+  useEffect(() => {
+    if (isOpen) {
+      props.onOpen?.();
+    } else {
+      props.onClose?.();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen]);
+
+  useEffect(() => {
+    props.onChange?.(value);
+  }, [value]);
 
   const handlePress = () => {
     if (props.disabled) return;
 
-    if (props.open) {
-      props.onClose?.();
-    } else {
-      props.onOpen?.();
-    }
+    setIsOpen(!isOpen);
   };
 
   const handleItemPress = (item: string) => {
     setValue(item);
-    props.onChange?.(item);
   };
 
   return (
@@ -32,29 +59,31 @@ export const Select: SelectComponent = (props) => {
       <Pressable onPress={handlePress} style={styles.select}>
         <ChevronIcon
           onPress={handlePress}
-          rotation={props.open ? 'Top' : 'Bottom'}
+          rotation={isOpen ? 'Top' : 'Bottom'}
           size={iconSize}
           style={styles.chevron}
         />
       </Pressable>
 
-      <DropdownList
-        containerRect={containerRect}
-        items={getTestSearchItems()}
-        renderItem={(item) => (
-          <Pressable
-            key={item.id}
-            onPress={() => handleItemPress(item.label)}
-            style={styles.suggestionItem}
-          >
-            {composePartialTextNode(item.label, {
-              matchedTextNodeStyle: { fontWeight: 'bold' },
-              startStrPart: MATCH_TAG_START,
-              endStrPart: MATCH_TAG_END,
-            })}
-          </Pressable>
-        )}
-      />
+      {isOpen && (
+        <DropdownList
+          containerRect={containerRect}
+          items={getTestSearchItems()}
+          renderItem={(item) => (
+            <Pressable
+              key={item.id}
+              onPress={() => handleItemPress(item.label)}
+              style={styles.suggestionItem}
+            >
+              {composePartialTextNode(item.label, {
+                matchedTextNodeStyle: { fontWeight: 'bold' },
+                startStrPart: MATCH_TAG_START,
+                endStrPart: MATCH_TAG_END,
+              })}
+            </Pressable>
+          )}
+        />
+      )}
     </View>
   );
 };
@@ -73,6 +102,7 @@ const styles = StyleSheet.create({
   },
   select: {
     width: '100%',
+    height: '100%',
   },
   chevron: { position: 'absolute', right: 4, top: 18 },
   suggestionItem: {
@@ -82,19 +112,3 @@ const styles = StyleSheet.create({
     borderColor: 'grey',
   },
 });
-
-interface SelectProps {
-  disabled?: boolean;
-  multiple?: boolean;
-  open?: boolean;
-  onOpen?: () => void;
-  onClose?: () => void;
-  onChange?: (value: string | string[]) => void;
-  placeholder?: string;
-  renderValue?: (value: string) => React.ReactNode;
-  testID?: string;
-  value?: string | string[];
-  icon?: React.ReactNode;
-}
-
-type SelectComponent = React.FC<SelectProps>;
