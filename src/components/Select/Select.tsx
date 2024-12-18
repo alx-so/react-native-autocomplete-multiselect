9d0b37e4a376af8b81b633fe6a0d758157a18c0a
alx-so <sokol.alxs@gmail.com>
2024-12-18 10:29:10 +0300

    feat: Selected filter out items from search results that are already selected

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -107,6 +107,14 @@ export const Select: React.FC<SelectProps> = (props) => {
       return newItemValue;
     });
 
+    if (props.searchable) {
+      setAutocompleteItems((prevItems) => {
+        const itemsWithoutSelected = prevItems.filter((i) => i.id !== item.id);
+
+        return itemsWithoutSelected;
+      });
+    }
+
     if (props.closeOnSelect) {
       setIsOpen(false);
     }
@@ -175,7 +183,12 @@ export const Select: React.FC<SelectProps> = (props) => {
   const handleSearchChange = (val: string) => {
     startItemsListTransition(() => {
       const _items = handleSearch(val as string);
-      setAutocompleteItems(_items);
+      console.log('handleSearchChange value', value);
+      const _itemsWithoutSelected = _items.filter(
+        (i) => !isItemSelected(removeTags(i.label), value)
+      );
+
+      setAutocompleteItems(_itemsWithoutSelected);
     });
   };
 
